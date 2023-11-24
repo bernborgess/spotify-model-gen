3e8be0570dcab05d5085d002bd75886b9b138117
bernborgess <bernborgess@outlook.com>
2023-11-24 21:09:38 +0000

    Training the model

--- gen.py
+++ gen.py
@@ -1,32 +1,25 @@
 import pandas as pd
 from fpgrowth_py import fpgrowth
-from sys import platform
-
+import os
 import pickle
 
 # Data file constants
-DATASETS_FOLDER = "/home/datasets"
-TRAIN_DATA_FILENAME = "2023_spotify_ds1.csv"
-UPDATE_DATA_FILENAME = "2023_spotify_ds2.csv"
-SONGS_FILENAME = "2023_spotify_songs.csv"
-
-# Persistent Volume Access
-if platform == "win32":
-    OUTPUT_FOLDER = "../shared"
-else:
-    OUTPUT_FOLDER = "/home/bernardoborges/project2-pv"
-
+VOLUME_DIR = os.environ.get("VOLUME_DIR")
+DATASET_FILENAME = os.environ.get("DATASET_FILENAME")
 OUTPUT_FILENAME = "model.pkl"
 
 
 def main():
+
+    # Testing the container
+    print(f"Take the dataset from {VOLUME_DIR}/{DATASET_FILENAME}")
+    print(f"Store it in {VOLUME_DIR}/{OUTPUT_FILENAME}")
+
     # Reading input file with relevant columns
     IN_COL = "track_name"
     OUT_COL = "pid"
 
-    df = pd.read_csv(
-        f"{DATASETS_FOLDER}/{TRAIN_DATA_FILENAME}", usecols=[IN_COL, OUT_COL]
-    )
+    df = pd.read_csv( f"{VOLUME_DIR}/{DATASET_FILENAME}", usecols=[IN_COL, OUT_COL])
 
     # Grouping tracks by artists
     gd = df.groupby(OUT_COL)[IN_COL].agg(list).reset_index()
@@ -40,7 +33,7 @@ def main():
 
     freqItemSet, rules = result
 
-    with open(f"{OUTPUT_FOLDER}/{OUTPUT_FILENAME}", "wb") as f:
+    with open(f"{VOLUME_DIR}/{OUTPUT_FILENAME}", "wb") as f:
         pickle.dump(rules, f)
 
 
