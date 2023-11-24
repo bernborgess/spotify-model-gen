229bdeaabaa4ff07c613a4956ee6e83bae016851
bernborgess <bernborgess@outlook.com>
2023-11-24 20:22:27 -0300

    Ultimate recommendation generator

--- gen.py
+++ gen.py
@@ -10,7 +10,6 @@ OUTPUT_FILENAME = "model.pkl"
 
 
 def main():
-
     # Testing the container
     print(f"Take the dataset from {VOLUME_DIR}/{DATASET_FILENAME}")
     print(f"Store it in {VOLUME_DIR}/{OUTPUT_FILENAME}")
@@ -18,20 +17,23 @@ def main():
     # Reading input file with relevant columns
     IN_COL = "track_name"
     OUT_COL = "pid"
+    df = pd.read_csv(f"{VOLUME_DIR}/{DATASET_FILENAME}", usecols=[IN_COL, OUT_COL])
 
-    df = pd.read_csv( f"{VOLUME_DIR}/{DATASET_FILENAME}", usecols=[IN_COL, OUT_COL])
+    # Map unique pids to indices
+    unique_pids = df[OUT_COL].unique()
+    pid_to_index = {pid: idx for idx, pid in enumerate(unique_pids)}
 
-    # Grouping tracks by artists
-    gd = df.groupby(OUT_COL)[IN_COL].agg(list).reset_index()
+    # Create a list of sets
+    sets_list = [set() for _ in unique_pids]
 
-    itemSetList = list(gd[IN_COL])
+    # Insert track_names into sets
+    for _, row in df.iterrows():
+        pid = row[OUT_COL]
+        track_name = row[IN_COL]
+        sets_list[pid_to_index[pid]].add(track_name)
 
-    result = fpgrowth(itemSetList, minSupRatio=0.01, minConf=0.2)
-
-    if result == None:
-        return
-
-    freqItemSet, rules = result
+    # Transform the list of sets into a list of lists
+    rules = [list(s) for s in sets_list]
 
     with open(f"{VOLUME_DIR}/{OUTPUT_FILENAME}", "wb") as f:
         pickle.dump(rules, f)
